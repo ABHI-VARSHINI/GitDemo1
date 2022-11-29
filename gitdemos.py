a296e8385513be1348b8f6554c7654a4c3c1784d
Maddala <abhivarshini.m@gmail.com>
2022-11-29 16:43:18 +0530

    Committed

--- gitdemos.py
+++ gitdemos.py
@@ -2,8 +2,4 @@ print("Hello")
 print("iNeuron")
 print("Line 3")
 print("written by Varsha")
-<<<<<<< HEAD
-print("Line 5 in local")
-=======
 print("Line 5 on GitHub")
->>>>>>> 3414aa859c4ae0352d5f945cc2c5147205d6e841
