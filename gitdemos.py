e47d5430e9653d184db678690d237c627fa3c47d
Maddala <abhivarshini.m@gmail.com>
2022-11-29 19:24:17 +0530

    New commit

---
+++ gitdemos.py
@@ -0,0 +1,13 @@
+print("Hello")
+print("iNeuron")
+print("Line 3")
+print("written by Varsha")
+print("Line 5 on GitHub")
+print("Line 6 on local")
+<<<<<<< HEAD
+print("Line 7 on local")
+print("Line 8 on local")
+print("ancdef")
+=======
+print("Line 7 on local")
+>>>>>>> b11a7192408e8e14a0196d1ef840cc30dea7bf9f
